48c5b1cfbe735fa96c6e8af91a976a2364048618
nexus <nexusgood93new@gmail.com>
2025-12-09 22:11:24 +0800

    Initial Commit

---
+++ 9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  9
+//
+//  Created by 陳詠平 on 2025/12/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 9/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  9
+//
+//  Created by 陳詠平 on 2025/12/9.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
